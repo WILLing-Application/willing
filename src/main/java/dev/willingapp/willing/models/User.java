66f6ebb84b58b953e1b5a45848a21251bd324c6a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-17 19:10:08 -0500

    created User model/Entity

---
+++ src/main/java/dev/willingapp/willing/models/User.java
@@ -0,0 +1,166 @@
+package dev.willingapp.willing.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 60, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 60, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 60)
+    private String firstName;
+
+    @Column(nullable = false, length = 60)
+    private String lastName;
+
+    @Column(nullable = false, length = 60)
+    private String address1;
+
+    @Column(length = 60)
+    private String address2;
+
+    @Column(nullable = false, length = 60)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String state;
+
+    @Column(nullable = false, length = 10)
+    private String zip;
+
+    @Column(nullable = false, length = 20)
+    private String phone;
+
+    @Column(columnDefinition = "TINYINT(1) UNSIGNED DEFAULT '1'")
+    private int allowSms;
+
+    @Column(columnDefinition = "TINYINT(1) UNSIGNED DEFAULT '0'")
+    private int isActive;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAllowSms() {
+        return allowSms;
+    }
+
+    public void setAllowSms(int allowSms) {
+        this.allowSms = allowSms;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+}
