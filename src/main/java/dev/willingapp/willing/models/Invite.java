b307f3cb7541ab659459f56cc0688ee68ec63cbd
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-17 21:49:48 -0500

    created Invite class/entity

---
+++ src/main/java/dev/willingapp/willing/models/Invite.java
@@ -0,0 +1,69 @@
+package dev.willingapp.willing.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "invites")
+public class Invite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 60)
+    private String email;
+
+    @Column(columnDefinition = "DATETIME NOT NULL")
+    private String createAt;
+
+    @Column(columnDefinition = "DATETIME NOT NULL")
+    private String updatedAt;
+
+//    Tied to album table
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    public Invite() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String emaail) {
+        this.email = emaail;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+}
