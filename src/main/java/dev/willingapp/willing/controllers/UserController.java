43b799b33dabe047f499c20a127607a82e362e10
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-19 11:29:44 -0500

    Implement Spring Boot Security

--- src/main/java/dev/willingapp/willing/controllers/UserController.java
+++ src/main/java/dev/willingapp/willing/controllers/UserController.java
@@ -1,31 +1,35 @@
 package dev.willingapp.willing.controllers;
 
+import dev.willingapp.willing.models.User;
+import dev.willingapp.willing.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
-//    private UserRepository users;
-//    private PasswordEncoder passwordEncoder;
-//
-//    public SignUpController(UserRepository users, PasswordEncoder passwordEncoder) {
-//        this.users = users;
-//        this.passwordEncoder = passwordEncoder;
-//    }
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
 
-//    @GetMapping("/sign-up")
-//    public String showSignupForm(Model model){
-////        model.addAttribute("user", new User());
-//        return "users/sign-up";
-//    }
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-//    @PostMapping("/sign-up")
-//    public String saveUser(@ModelAttribute User user){
-//        String hash = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(hash);
-//        users.save(user);
-//        return "redirect:/login";
-//    }
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
 }
--- src/main/java/dev/willingapp/willing/repositories/UserRepository.java
+++ src/main/java/dev/willingapp/willing/repositories/UserRepository.java
@@ -3,5 +3,7 @@ package dev.willingapp.willing.repositories;
 import dev.willingapp.willing.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> { // curriculum has repo name Users
+    // Spring Security needs this to authenticate users
+    User findByUsername(String username);
 }
