03b028c4127c96437b127c191705108db3ae6805
Greg Jett <greg.jett25@gmail.com>
2020-08-22 17:48:56 -0500

    got create album working

--- src/main/java/dev/willingapp/willing/repositories/AlbumRepository.java
+++ src/main/java/dev/willingapp/willing/repositories/AlbumRepository.java
@@ -3,7 +3,10 @@ package dev.willingapp.willing.repositories;
 import dev.willingapp.willing.models.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+    List<Album> findAllByOrderByIdDesc();
 }
 
 
