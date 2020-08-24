2297c4a694ace69459005d71c7a73c1714dabfb4
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-23 21:12:36 -0500

    initial user profile view

--- src/main/java/dev/willingapp/willing/controllers/ProfileController.java
+++ src/main/java/dev/willingapp/willing/controllers/ProfileController.java
@@ -1,15 +1,38 @@
 package dev.willingapp.willing.controllers;
 
+import dev.willingapp.willing.models.User;
+import dev.willingapp.willing.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProfileController {
+
+    private final UserRepository usersDao;
+
+    public ProfileController(UserRepository usersDao){
+        this.usersDao = usersDao;
+    }
+
+//    @GetMapping("/profile")
+//    public String getProfile(Model model){
+//        return "profile";
+//    }
+
+    // view logged in user profile
     @GetMapping("/profile")
-    public String returnLandingMessage(){
+    public String showUserProfile(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", usersDao.getOne(user.getId()));
+        if (user.getProfilePhoto() == null) {
+            user.setProfilePhoto("/photos/placeholder.jpg");
+        }
         return "profile";
     }
+
 }
