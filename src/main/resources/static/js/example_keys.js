a81c0b8beaa9d1dd5d414a38013d0e2e13f230b5
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-24 12:05:55 -0500

    add const

--- src/main/resources/static/js/example_keys.js
+++ src/main/resources/static/js/example_keys.js
@@ -1,3 +1,3 @@
 // THIS IS AN EXAMPLE FILE
 // Copy the line below to a new file named keys.js - input your own API keys into keys.js
-FILESTACK_API_KEY='put your key here';
+const FILESTACK_API_KEY='put your key here';
