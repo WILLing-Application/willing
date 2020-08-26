53347b86b7b8e74c3ef1a46308d81804aa303128
George Williams <george412williams@gmail.com>
2020-08-26 14:36:30 -0500

    table added to view page controller and repos adjusted

--- src/main/java/dev/willingapp/willing/controllers/ViewController.java
+++ src/main/java/dev/willingapp/willing/controllers/ViewController.java
@@ -1,17 +1,39 @@
 package dev.willingapp.willing.controllers;
 
 
+import dev.willingapp.willing.models.Album;
+import dev.willingapp.willing.models.User;
+import dev.willingapp.willing.repositories.AlbumRepository;
+import dev.willingapp.willing.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ViewController {
 
-    public ViewController(){}
+    private final AlbumRepository albumsDao;
+    private final UserRepository usersDao;
+
+    public ViewController(AlbumRepository albumsDao, UserRepository usersDao){
+        this.albumsDao = albumsDao;
+        this.usersDao = usersDao;
+    }
 
     @GetMapping("/view")
-    public String showView(Model model) {return "view";}
+    public String showView(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Album> albumsList = albumsDao.findAllByOwner(user);
+        model.addAttribute("albums", albumsList);
+        System.out.println(albumsList);
+        model.addAttribute("users", usersDao.findAll());
+        model.addAttribute("user", usersDao.getOne(user.getId()));
+//        for later, get all users of that album, of same album ID
+        return "view";
+    }
 
 
 
--- src/main/java/dev/willingapp/willing/repositories/AlbumRepository.java
+++ src/main/java/dev/willingapp/willing/repositories/AlbumRepository.java
@@ -1,13 +1,17 @@
 package dev.willingapp.willing.repositories;
 
 import dev.willingapp.willing.models.Album;
+import dev.willingapp.willing.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     List<Album> findAllByOrderByIdDesc();
-    List<Album> findAlbumsByUsersId(long id);
+//    List<Album> findAlbumsByUsersId(long id);
+    List<Album> findAlbumsByUsersId(Long id);
+    List<Album> findAllByUsersId(Long id);
+    List<Album> findAllByOwner(User user);
 }
 
 
