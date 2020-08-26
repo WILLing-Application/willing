f2278e26b1222df43d781c57c5ea466d2ab30416
Greg Jett <greg.jett25@gmail.com>
2020-08-26 16:12:14 -0500

    polished the album view and fixed javascript link

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,15 @@
+"use strict";
+// alert("hello")
+//     .card {
+//     max-width: 100%;
+//     overflow: hidden;
+// }
+//
+// .card-block {
+//     word-wrap: break-word;
+// }
+//
+// .card-text {
+//     word-wrap: break-word;
+// }
+
