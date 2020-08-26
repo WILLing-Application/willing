87e7994ccf2be428e0da6fd3cd68d6fef2ef7023
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-25 21:20:18 -0500

    starting profile edit

--- src/main/java/dev/willingapp/willing/controllers/ProfileController.java
+++ src/main/java/dev/willingapp/willing/controllers/ProfileController.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProfileController {
@@ -16,26 +18,32 @@ public class ProfileController {
         this.usersDao = usersDao;
     }
 
-//    @GetMapping("/profile")
-//    public String getProfile(Model model){
-//        return "profile";
-//    }
-
     // view logged in user profile
     @GetMapping("/profile")
-    public String showUserProfile(Model model) throws NullPointerException {
+    public String showUserProfile(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user", usersDao.getOne(user.getId()));
-        System.out.println("User profile_photo: " + user.getProfilePhoto()); // DEBUG
         return "users/profile";
     }
 
-//    @GetMapping("/profile/edit")
-//    public String editUserProfile(Model model) throws NullPointerException {
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        model.addAttribute("user", usersDao.getOne(user.getId()));
-//        System.out.println("User profile_photo: " + user.getProfilePhoto()); // DEBUG
-//        return "/users/profile";
-//    }
+    @GetMapping("/profile/edit")
+    public String editUserProfile(Model model) throws NullPointerException {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", usersDao.getOne(user.getId()));
+        return "users/edit";
+    }
+
+    @PostMapping("/profile/edit")
+    public String editUserDetail(@RequestParam(name="username") String username,
+                                 @RequestParam(name="password") String password,
+                                 @RequestParam(name="email") String email,
+                                 @RequestParam(name="phone") String phone, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", usersDao.getOne(user.getId()));
+        // set attributes from form
+        return "users/edit";
+
+    }
+
 
 }
