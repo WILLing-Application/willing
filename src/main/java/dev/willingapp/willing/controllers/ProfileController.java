69ab476df64b527a0f757cf2c8f4001727824c64
Laura Davis <laura.l.davis.210@gmail.com>
2020-08-27 02:23:21 -0500

    User profile Edit (name, address, phone) working

--- src/main/java/dev/willingapp/willing/controllers/ProfileController.java
+++ src/main/java/dev/willingapp/willing/controllers/ProfileController.java
@@ -20,71 +20,57 @@ public class ProfileController {
     @GetMapping("/profile")
     public String showUserProfile(Model model) {
         User myUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        model.addAttribute("user", usersDao.getOne(myUser.getId()));
-        model.addAttribute("user", myUser);
+        model.addAttribute("user", usersDao.getOne(myUser.getId()));
         return "users/profile";
     }
 
     // EDIT USER PROFILE (FORM)
     @GetMapping("/profile/{id}/edit")
     public String editUserProfile(@PathVariable long id, Model model) {
-        User myUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user", usersDao.getOne(myUser.getId()));
-
-//        boolean isDifferent = false;
-
+//        User myUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", usersDao.getOne(id));
         return "users/edit";
     }
-
-        // snippet from AlbumController & add-guest.html
-//        if (usersDao.findByEmail(email) != null) {
-//            user = usersDao.findByEmail(email);
-//            isNotUnique = false;
-//            model.addAttribute("user", user);
-//            model.addAttribute("isRegistered", isRegistered);
-//        } else if (usersDao.findByFirstName(firstName) != null && usersDao.findByLastName(lastName) != null) {
-//            user = usersDao.findByFirstName(firstName);
-//            isRegistered = true;
-//            model.addAttribute("user", user);
-//            model.addAttribute("isRegistered", isRegistered);
-//        } else {
-//            isRegistered = false;
-//            model.addAttribute("userNotFound", userNotFound);
-//            model.addAttribute("isRegistered", isRegistered);
-//        }
-
-
-    // snippet from user controller
-//    String hash = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(hash);
-//        usersDao.save(user);
-
 
     // EDIT/SAVE USER PROFILE
     @PostMapping("/profile/{id}/edit")
-    public String saveUserProfile(@RequestParam(name="username") String username,
-                                 @RequestParam(name="password") String password,
-                                 @RequestParam(name="email") String email,
-                                 @RequestParam(name="phone") String phone, Model model) {
+    public String saveUserProfile(@RequestParam(name="edit-user") long id,
+                                  @RequestParam(name="firstName") String firstName,
+                                  @RequestParam(name="lastName") String lastName,
+                                  @RequestParam(name="address1") String address1,
+                                  @RequestParam(name="address2") String address2,
+                                  @RequestParam(name="city") String city,
+                                  @RequestParam(name="state") String state,
+                                  @RequestParam(name="zip") String zip,
+                                  @RequestParam(name="phone") String phone,
+                                  Model model) {
         User myUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user", usersDao.getOne(myUser.getId()));
-        // check and set all the attributes sent with the form
-        return "users/edit";
-
+        User user = usersDao.getOne(myUser.getId());
+        user.setId(id);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAddress1(address1);
+        user.setAddress2(address2);
+        user.setCity(city);
+        user.setState(state);
+        user.setZip(zip);
+        user.setPhone(phone);
+        usersDao.save(user);
+        return "redirect:/profile";
     }
 
     @GetMapping("/profile/delete")
-    public String deleteProfileById(Model model) {
+    public String deleteProfile(Model model) {
         User myUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user", usersDao.getOne(myUser.getId()));
         System.out.println("value of myUser id: " + myUser.getId()); // DEBUG
         return "users/delete";
     }
 
+    // no errors, but doesn't remove user from the database
     @PostMapping("/profile/delete")
-    public String deleteProfile(@RequestParam(name="delete-user") long id) {
+    public String deleteProfileById(@RequestParam(name="delete-user") long id) {
         usersDao.deleteById(id);
         return "redirect:/";
     }
-
 }
